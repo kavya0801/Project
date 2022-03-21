2ec26c2d4f1c0f5e2f5b830705fc9590a68d0791
Vipin Kumar <vipin.kumar@moengage.com>
2022-03-21 16:36:18 +0530

    wip

--- app/src/main/java/com/app1/lisview/MainActivity.kt
+++ app/src/main/java/com/app1/lisview/MainActivity.kt
@@ -1,35 +1,25 @@
 package com.app1.lisview
 
-import com.app1.lisview.Network.getNews
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.app1.lisview.R
+import com.app1.lisview.Network.getNews
+import com.google.gson.JsonElement
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    var data = arrayOf(
-        "News",
-        "Myntra",
-        "Shopee",
-        "JioMart",
-        "Flipkart",
-        "Redbus",
-        "News",
-        "Myntra",
-        "Shopee",
-        "JioMart",
-        "Flipkart",
-        "Redbus"
-    )
-    var listView: ListView? = null
+
+    lateinit var response: JsonElement
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycleScope.launch{
-            getNews()
+        lifecycleScope.launch {
+            response = getNews()
         }
+
+        //TODO parse the response from the network and out it in an Arraylist
 
     }
 }
